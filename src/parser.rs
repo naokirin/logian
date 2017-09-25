3e581404eb62c8882f29b2b8fcaf468eeeb2d972
naokirin <naoki.rin186@gmail.com>
2017-09-26 03:52:56 +0900

    Add parse and render function

---
+++ src/parser.rs
@@ -0,0 +1,36 @@
+use std::path::Path;
+
+use ::file::*;
+use ::template::*;
+use ::schema::*;
+use ::json::*;
+
+pub fn parse_user_defined_types(template_dir: &str) -> Vec<::schema::data_type::DataType> {
+    let type_files = ::file::reader::read_glob(&format!("{}/**/*.json", template_dir)[..]);
+    type_files.into_iter().map(|path| {
+        let json = ::file::reader::read(&path[..]).unwrap();
+        let path = Path::new(&path[..]);
+        let type_name = path.file_stem().unwrap().to_str().unwrap();
+        ::schema::parser::parse_user_defined_type(type_name, &json[..])
+    }).collect()
+}
+
+pub fn parse_log_schemas(template_dir: &str, user_defined_types: &Vec<::schema::data_type::DataType>) -> Vec<::schema::data_type::LogSchema> {
+    let schema_files = ::file::reader::read_glob(&format!("{}/**/*.json", template_dir)[..]);
+    schema_files.into_iter().map(|path| {
+        let json = ::file::reader::read(&path[..]).unwrap();
+        let path = Path::new(&path[..]);
+        let log_name = path.file_stem().unwrap().to_str().unwrap();
+        ::schema::parser::parse_log_schema(log_name, &json[..], &user_defined_types)
+    }).collect()
+}
+
+pub fn parse_default_log_schema(template_path: &str, user_defined_types: &Vec<::schema::data_type::DataType>) -> ::schema::data_type::DefaultLogSchema {
+    let schema = ::file::reader::read(template_path);
+    if schema.is_err() {
+        panic!("No such default schema file: {}.", template_path);
+    }
+    let json = schema.unwrap();
+    ::schema::parser::parse_default_log_schema(&json[..], &user_defined_types)
+}
+
